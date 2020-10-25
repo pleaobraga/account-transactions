ad7680f1a72f988b0394e84de8e98f47061a1535
pleaobraga <pleao.braga@gmail.com>
2020-10-25 17:03:42 -0300

    Create HourFormat test

--- src/components/Atom/HoursFormat/HoursFormat.test.js
+++ src/components/Atom/HoursFormat/HoursFormat.test.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import Card from './Card'
+import { mount } from 'enzyme'
+import HoursFormat from './HoursFormat'
 
-describe('Card', () => {
-  const card = shallow(<Card title="Test" text="test" />)
+describe('Hours Format', () => {
+  const date = new Date()
+  const wrapper = mount(<HoursFormat date={date} />)
 
-  it('render proprely', () => {
-    expect(card).toMatchSnapshot()
+  it('render wrapper proprely', () => {
+    expect(wrapper.find('.hours-format').length).toBe(1)
+  })
+
+  it('render icon proprely', () => {
+    expect(wrapper.find('.hours-format__icon').length).toBe(1)
+  })
+
+  it('render time proprely', () => {
+    expect(wrapper.find('.hours-format__time').length).toBe(1)
   })
 })
