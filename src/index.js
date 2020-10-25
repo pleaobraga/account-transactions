c593932416c1083f314fba3103bc50b41e138339
pleaobraga <pleao.braga@gmail.com>
2020-10-25 13:30:45 -0300

    Remove redux configuration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import ReduxThunk from 'redux-thunk'
-import rootReducers from './redux'
-import './styles/_main.scss'
 import Routes from './routes'
+import './styles/_main.scss'
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose
-
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk))
-
-const store = createStore(rootReducers, enhancer)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<Routes />, document.getElementById('root'))
