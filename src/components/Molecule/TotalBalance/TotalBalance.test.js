9c627eb2a45893199fe178b400011d0f62becbca
pleaobraga <pleao.braga@gmail.com>
2020-10-25 18:16:50 -0300

    Create TotalBalance Test

--- src/components/Molecule/TotalBalance/TotalBalance.test.js
+++ src/components/Molecule/TotalBalance/TotalBalance.test.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Card from './Card'
+import TotalBalance from './TotalBalance'
 
-describe('Card', () => {
-  const card = shallow(<Card title="Test" text="test" />)
+describe('Total Balance', () => {
+  const wrapper = shallow(<TotalBalance amount={100} />)
 
-  it('render properly', () => {
-    expect(card).toMatchSnapshot()
+  it('should render component properly', () => {
+    expect(wrapper.find('.balance').length).toBe(1)
+  })
+
+  describe('Should render title', () => {
+    it('should render properly', () => {
+      expect(wrapper.find('.balance__title').length).toBe(1)
+    })
+
+    it('should render title text ', () => {
+      const title = wrapper.find('.balance__title')
+      expect(title.text()).toEqual('Saldo Total')
+    })
+  })
+
+  describe('Should render amount', () => {
+    it('should render properly', () => {
+      expect(wrapper.find('.balance__value').length).toBe(1)
+    })
+
+    it('should render amount value ', () => {
+      const amount = wrapper.find('.balance__value')
+      expect(amount.text()).toEqual('R$100.00')
+    })
   })
 })
